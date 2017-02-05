9d6872499be623c2a895fc0c01f8db72b40e2a4b
Vmk9001 <Vmk9002@gmail.com>
2017-02-05 00:48:15 -0600

    Add files via upload

---
+++ BubbleLoad/js/config.js
@@ -0,0 +1,34 @@
+/*
+	BubbleLoad
+	v1.00 by Lachlan(http://steamcommunity.com/id/UnresolvedVice)
+*/
+
+// Display Servername (If false, it will be automatically detected)
+var displayName = true;
+// Defined Servername
+var serverName = "EndorPointGaming-StarWars Server";
+
+// Backgrounds (Will be looked for in backgrounds/)
+var backgrounds = [
+	"background1.png",
+	"background2.png",
+	"background3.png",
+	"background4.png",
+	"background5.png",
+];
+
+// Server Rules (A bit buggy right now)
+var serverRules = [
+	"Do NOT RDM (Never Kill A Brother)",
+	"Don't Be Disrespectful TO Staff (Where Trying To Help)",
+	"Don't Spam Chat",
+	"Don't Be A Troll (You Will Be Band For 24Hours)",
+	"This Is A Semi Serious Star Wars Server",
+];
+
+// If false, music will be disabled
+var useMusic = true;
+// Music Playlist (ogg, will be looked for in audio/)
+var musicPlaylist = [
+	{name: "Tiimmy Turner ", author: "Desiigner", file:"1.ogg"},
+];
